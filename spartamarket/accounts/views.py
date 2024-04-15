2a5fd9d468e0034a1650a436353d7fc075b37ca2
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-04-15 17:38:27 +0900

    회원정보

--- spartamarket/accounts/views.py
+++ spartamarket/accounts/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.http import require_POST, require_http_methods
+from django.contrib.auth.forms import UserCreationForm
 
 
+@require_http_methods(["GET", "POST"])
 def login(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("articles:index ")
     else:
         form = AuthenticationForm()
-        context = {"form": form}
-    return render(request, 'accounts/login.html', context)
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
+
+@require_POST
+def logout(request):
+    auth_logout(request)
+    return render(request, "accounts/logout.html")
+
+
+@require_http_methods(["GET", "POST"])
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(require_POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
