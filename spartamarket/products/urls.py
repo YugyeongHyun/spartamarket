5d3bf3f6c23824f664a8471f87547e9578be7100
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-04-16 17:58:48 +0900

    CRUD 초안 완성

--- spartamarket/products/urls.py
+++ spartamarket/products/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "products"
 urlpatterns = [
-    path('', views.products, name='products')
+    path("", views.articles, name="articles"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
+    path("<int:pk>/edit/", views.edit, name="edit"),
+    path("<int:pk>/update/", views.update, name="update"),
 ]
--- spartamarket/products/views.py
+++ spartamarket/products/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Article
 
 
-def products(request):
-    response = HttpResponse("<h1> 나 products야 </h1>")
-    return render(request, "products.html")
+def articles(request):
+    articles = Article.objects.all().order_by("-created_at")
+    context = {"articles": articles, }
+    return render(request, "articles.html", context)
+
+
+def new(request):
+    return render(request, "new.html")
+
+
+def create(request):
+
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+
+    article = Article(title=title, content=content)
+    article.save()
+    context = {
+        "article": article,
+    }
+    return redirect("products:articles")
+
+
+def detail(request, pk):
+    article = Article.objects.get(id=pk)
+    context = {"article": article}
+    return render(request, "detail.html", context)
+
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect("products:articles")
+
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {"article": article}
+    return render(request, "edit.html", context)
+
+
+def update(request, pk):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+
+    article = Article.objects.get(pk=pk)
+    article.content = content
+    article.title = title
+    article.save()
+    return redirect("products:detail", article.pk)
